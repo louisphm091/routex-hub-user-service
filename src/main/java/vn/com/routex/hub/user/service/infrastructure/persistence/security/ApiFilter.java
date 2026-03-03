1d95c60c5365211f53381f936c1764e0a892ee8a
Bao Pham <louis.phammm@gmail.com>
2026-03-03 14:06:52 +0700

    Update spring boot 4.0 & security check for request filter

--- src/main/java/vn/com/routex/hub/user/service/infrastructure/persistence/security/ApiFilter.java
+++ src/main/java/vn/com/routex/hub/user/service/infrastructure/persistence/security/ApiFilter.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Request;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 import vn.com.routex.hub.user.service.application.RequestAttributes;
 import vn.com.routex.hub.user.service.interfaces.models.base.BaseRequest;
 
@@ -26,18 +27,23 @@ public class ApiFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
-        if (requestURI.startsWith("/actuator/")) {
+
+        if (requestURI.startsWith("/actuator/") || requestURI.startsWith("/swagger-ui") || requestURI.startsWith("/v3/api-docs")) {
             filterChain.doFilter(request, response);
             return;
         }
         try {
             CachedHttpServletRequestWrapper cachedHttpServletRequestWrapper = new CachedHttpServletRequestWrapper(request);
+            ContentCachingResponseWrapper contentCachingResponseWrapper = new ContentCachingResponseWrapper(response);
             String jsonStringBody = new String(cachedHttpServletRequestWrapper.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
             BaseRequest apiRequest = objectMapper.readValue(jsonStringBody, BaseRequest.class);
             request.setAttribute(RequestAttributes.REQUEST_ID, apiRequest.getRequestId());
             request.setAttribute(RequestAttributes.REQUEST_DATE_TIME, apiRequest.getRequestDateTime());
             request.setAttribute(RequestAttributes.CHANNEL, apiRequest.getChannel());
-            filterChain.doFilter(cachedHttpServletRequestWrapper, response);
+            filterChain.doFilter(cachedHttpServletRequestWrapper, contentCachingResponseWrapper);
+            String responseMessage = new String(contentCachingResponseWrapper.getContentAsByteArray(), StandardCharsets.UTF_8);
+            log.info("{}", responseMessage);
+            contentCachingResponseWrapper.copyBodyToResponse();
         } catch (Exception e) {
             log.error(e.getLocalizedMessage());
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
